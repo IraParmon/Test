1172824f2a46b32632d78d7c7f85a3d61e93a033
IraParmon <rena7191@mail.ru>
2022-07-03 13:42:19 +0300

    1

--- 2/1.py
+++ 2/1.py
@@ -1,2 +1,17 @@
-a=1
-print(a)
+a = input().split()
+l = []
+for i in range(len(a)):
+    l.append(a[i])
+print(l)
+
+
+l = [int(x) for x in a]       #преобразование каждого элемента списка из строки в число
+
+for i in l:
+    if i % 3 == 0 and i % 5 != 0:
+        print("Число подходит под условия:",i)
+
+max_number = max(l)
+print("Максимальное:", max_number)
+min_number = min(l)
+print("Минимальное:", min_number)
