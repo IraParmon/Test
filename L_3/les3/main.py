18bf4fed28bff5dc78d655e1fe2d20ab95792805
IraParmon <rena7191@mail.ru>
2022-07-11 22:44:58 +0300

    1

---
+++ L_3/les3/main.py
@@ -0,0 +1,8 @@
+from L_3.les3.fun_Date import read_input, fun_date, fun_correct
+
+dd, mm, yyyy = read_input()
+
+
+print(fun_date(dd, mm, yyyy))
+
+print(fun_correct(dd, mm, yyyy))
---
+++ L_3/les3_2/main.py
@@ -0,0 +1,3 @@
+from L_3.les3_2.trans import read_input
+
+print(read_input())
