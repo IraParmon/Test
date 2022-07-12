50fa5f737f18c65578afe245be51cffbcfcfa63d
IraParmon <rena7191@mail.ru>
2022-07-12 19:18:41 +0300

    1

--- cl_les5/main.py
+++ cl_les5/main.py
@@ -2,10 +2,14 @@
 # Выведите все локальные максимумы (элементы с
 #края не учитываются)
 
-my_list = [1, 16, 5, 4, 646, 153]
+my_list = [1, 4, 2, 6, 3, 10, 9]
 if my_list[0] < my_list[1] > my_list[2]:
     print(my_list[1])
 if my_list[1] < my_list[2] > my_list[3]:
     print(my_list[2])
+if my_list[2] < my_list[3] > my_list[4]:
+    print(my_list[3])
 if my_list[3] < my_list[4] > my_list[5]:
     print(my_list[4])
+if my_list[4] < my_list[5] > my_list[6]:
+    print(my_list[5])
